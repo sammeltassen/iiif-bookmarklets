abf8dfc4cf52a4a5d0f57df1fd6d7cdfb44ae9b7
Jules Schoonman <jules.schoonman@gmail.com>
2023-09-21 10:47:38 +0200

    removed decoding step

--- _includes/manifest-param.js
+++ _includes/manifest-param.js
@@ -1,13 +1,12 @@
 javascript: (function () {
-    // Split params from url and get the manifest param
-    const params = window.location.href.split('?')[1];
-    const encodedManifest = new URLSearchParams(params).get('manifest');
-    // Decode param and navigate to manifest
-    if (encodedManifest) {
-        const decodedManifest = decodeURIComponent(encodedManifest);
-        location = decodedManifest
+    // Split params from url and get the manifest or url param
+    const params = new URLSearchParams(window.location.href.split("?")[1]);
+    const manifestUri = params.get("manifest") || params.get("url");
+    // Navigate to manifest
+    if (manifestUri) {
+        window.location = manifestUri;
     } else {
         // Or alert the user that no manifest was found
-        window.alert('No manifest found!')
+        window.alert("No manifest found!");
     }
 })()
